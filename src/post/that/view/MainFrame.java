142a0c45309121bc47501157fa865ed6e25c7232
Louis Parent <contact@louis-parent.fr>
2021-02-23 23:59:04 +0100

    Adding tool bar

--- src/post/that/view/MainFrame.java
+++ src/post/that/view/MainFrame.java
@@ -2,14 +2,18 @@ package post.that.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
 import javax.swing.WindowConstants;
 
 import post.that.utils.Translation.Internationalization;
@@ -35,55 +39,6 @@ public class MainFrame extends JFrame implements WindowAdapter
 		this.setVisible(true);
 	}
 
-	private void setup()
-	{
-		this.setTitle(MainFrame.TITLE);
-		this.setSize(MainFrame.DEFAULT_SIZE);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-		this.addWindowListener(this);
-	}
-
-	private void build()
-	{
-		this.board = new BoardPane();
-
-		this.setLayout(new BorderLayout());
-		this.add(this.board, BorderLayout.CENTER);
-		
-		this.setJMenuBar(this.buildMenuBar());
-	}
-
-	private JMenuBar buildMenuBar()
-	{
-		JMenuBar menuBar = new JMenuBar();
-		
-		JMenu itemMenu = new JMenu(Internationalization.get("FILE"));
-		itemMenu.setMnemonic(KeyEvent.VK_P);
-		menuBar.add(itemMenu);
-		
-		JMenuItem addPostThatItem = new JMenuItem(Internationalization.get("CREATE_POST_THAT"));
-		addPostThatItem.setMnemonic(KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_N);
-		addPostThatItem.setIcon(Images.NOTE_ICON.getScaledIcon(16, 16));
-		addPostThatItem.addActionListener(event -> {
-			this.board.createEmptyPostThat();
-		});
-		itemMenu.add(addPostThatItem);
-		
-		JMenuItem saveItem = new JMenuItem(Internationalization.get("SAVE"));
-		saveItem.setMnemonic(KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_S);
-		saveItem.setIcon(Images.SAVE_ICON.getScaledIcon(16, 16));
-		saveItem.addActionListener(event -> {
-			if(!this.board.save())
-			{
-				JOptionPane.showMessageDialog(this, Internationalization.get("UNABLE_TO_SAVE_BOARD"), Internationalization.get("SAVE_ERROR"), JOptionPane.WARNING_MESSAGE);
-			}
-		});
-		itemMenu.add(saveItem);
-		
-		return menuBar;
-	}
-
 	@Override
 	public void windowClosing(WindowEvent event)
 	{
@@ -99,5 +54,77 @@ public class MainFrame extends JFrame implements WindowAdapter
 				System.exit(1);
 			}
 		}
+	}
+
+	private void setup()
+	{
+		this.setTitle(MainFrame.TITLE);
+		this.setSize(MainFrame.DEFAULT_SIZE);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		this.addWindowListener(this);
+	}
+
+	private void build()
+	{
+		this.board = new BoardPane();
+		this.setJMenuBar(this.buildMenuBar());
+
+		this.setLayout(new BorderLayout());
+		this.add(this.buildToolBar(), BorderLayout.NORTH);
+		this.add(this.board, BorderLayout.CENTER);
+
+	}
+
+	private JMenuBar buildMenuBar()
+	{
+		JMenuBar menuBar = new JMenuBar();
+
+		JMenu itemMenu = new JMenu(Internationalization.get("FILE"));
+		itemMenu.setMnemonic(KeyEvent.VK_P);
+		menuBar.add(itemMenu);
+
+		JMenuItem addPostThatItem = new JMenuItem(Internationalization.get("CREATE_POST_THAT"));
+		addPostThatItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+		addPostThatItem.setIcon(Images.NOTE_ICON.getScaledIcon(16, 16));
+		addPostThatItem.addActionListener(event -> {
+			this.board.createEmptyPostThat();
+		});
+		itemMenu.add(addPostThatItem);
+
+		JMenuItem saveItem = new JMenuItem(Internationalization.get("SAVE"));
+		saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+		saveItem.setIcon(Images.SAVE_ICON.getScaledIcon(16, 16));
+		saveItem.addActionListener(event -> {
+			if(!this.board.save())
+			{
+				JOptionPane.showMessageDialog(this, Internationalization.get("UNABLE_TO_SAVE_BOARD"), Internationalization.get("SAVE_ERROR"), JOptionPane.WARNING_MESSAGE);
+			}
+		});
+		itemMenu.add(saveItem);
+
+		return menuBar;
+	}
+
+	private JToolBar buildToolBar()
+	{
+		JToolBar toolBar = new JToolBar(Internationalization.get("BOARD_MANAGEMENT"));
+
+		JButton addPostThatButton = new JButton(Images.NOTE_ICON.getScaledIcon(16, 16));
+		addPostThatButton.addActionListener(event -> {
+			this.board.createEmptyPostThat();
+		});
+		toolBar.add(addPostThatButton);
+		
+		JButton saveButton = new JButton(Images.SAVE_ICON.getScaledIcon(16, 16));
+		saveButton.addActionListener(event -> {
+			if(!this.board.save())
+			{
+				JOptionPane.showMessageDialog(this, Internationalization.get("UNABLE_TO_SAVE_BOARD"), Internationalization.get("SAVE_ERROR"), JOptionPane.WARNING_MESSAGE);
+			}
+		});
+		toolBar.add(saveButton);
+
+		return toolBar;
 	}
 }
