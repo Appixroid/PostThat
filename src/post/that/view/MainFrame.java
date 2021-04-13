1b1fd7fb4f781106516030f496609d91875e8d37
Louis Parent <contact@louis-parent.fr>
2021-04-13 19:03:16 +0200

    Adding text for save all

--- src/post/that/view/MainFrame.java
+++ src/post/that/view/MainFrame.java
@@ -158,7 +158,7 @@ public class MainFrame extends JFrame implements WindowAdapter
 
 	private JMenuItem createSaveAllItem()
 	{
-		JMenuItem saveAllItem = new JMenuItem(Internationalization.get("SAVE"));
+		JMenuItem saveAllItem = new JMenuItem(Internationalization.get("SAVE_ALL"));
 		saveAllItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK));
 		saveAllItem.setIcon(Images.SAVE_ICON.getScaledIcon(16, 16));
 		saveAllItem.addActionListener(event -> {
